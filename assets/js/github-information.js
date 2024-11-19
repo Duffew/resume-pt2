9b443a2d4ce984b0627f7f32d79161d88098eae2
Nicholas Duffew <pbsvimqx@students.codeinstitute.net>
2024-11-19 08:43:17 +0000

    feat: add jquery promise - $.when().then()

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -14,4 +14,26 @@ function fetchGitHubInformation(event) {
             <img src="assets/css/loader.gif" alt="loading..." />
         </div>`);
 
+    // create a promise
+    // when this username is requested...
+    $when(
+        $.getJSON(`https://api.github.com/users/${username}`)
+    ).then(
+        // run this function to display the GitHub response
+        function(response) {
+            var userData = response;
+            $("#gh-user-data").html(userInformationHTML(userData));
+            // in case of 400 error run this function
+        }, function(errorResponse) {
+            if (errorResponse.status === 400) {
+                $("#gh-user-data").html(
+                    `<h2>User ${username} not found</h2>`);
+            // if not 400 error, display JSON error message in the console
+            } else {
+                console.log(errorResponse);
+                $("#gh-user-data").html(
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+            }
+        });
+
 }
