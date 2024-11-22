f5113a84e4fd4a901b51d776006b314230eab2b1
Nicholas Duffew <pbsvimqx@students.codeinstitute.net>
2024-11-22 14:34:24 +0000

    feat: add sendEmail.js and sendEmail()

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,19 @@
-console.log("hi!");
+function sendEmail(contactForm) {
+    emailjs.send("service_1deey6f","template_slwyad7", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    // create a promise for the send function
+    .then(
+        // if the function is a success
+        function (response) {
+            // log the message ""success" and pass in the response message
+            console.log("SUCCESS", response)
+        },
+        // if there is an error
+        function(error) {
+            console.log("FAILED", error);
+        });
+
+}
